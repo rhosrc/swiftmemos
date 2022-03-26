dfbf0f22c4c15afd53d8ab10095aed74935caf60
Chris Lafortune <chris.lafortune@gmail.com>
2022-03-25 22:06:53 -0400

    tweaked static url

--- project4/settings.py
+++ project4/settings.py
@@ -119,7 +119,7 @@ DATE_FORMAT = '%d-%m-%y'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/memos/'
 LOGOUT_REDIRECT_URL = '/'
+
+import django_heroku
+django_heroku.settings(locals())
